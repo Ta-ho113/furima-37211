dc7ac9442505a1dbf905fa4804a0d560bb770163
Ta-ho113 <takuma.04.soka@gmail.com>
2022-04-11 23:57:55 +0900

    ユーザー登録機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nickname,              presence: true
+  validates :email,                 presence: true
+  validates :encrypted_password,    presence: true
+  validates :password_confirmation, presence: true
+  validates :last_name,             presence: true
+  validates :first_name,            presence: true
+  validates :last_katakana,         presence: true
+  validates :first_katakana,        presence: true
+  validates :birth_day_id,          presence: true
+  has_many :purchased_items,        dependent: :destroy
+  has_many :comments,               dependent: :destroy
+  has_many :items,                  dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -16,10 +16,10 @@ ActiveRecord::Schema.define(version: 2022_04_05_055952) do
     t.string "nickname", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "last_name", null: false
-    t.string "first_name", null: false
-    t.string "last_katakana", null: false
-    t.string "first_katakana", null: false
+    t.string "last_name", default: "", null: false
+    t.string "first_name", default: "", null: false
+    t.string "last_katakana", default: "", null: false
+    t.string "first_katakana", default: "", null: false
     t.date "birth_day_id", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
