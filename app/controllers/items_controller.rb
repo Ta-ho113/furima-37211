f350e0bf7001d49c910f44e11d7a8d1d06f8a12c
Ta-ho113 <takuma.04.soka@gmail.com>
2022-04-19 02:46:59 +0900

    商品出品ページの表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,20 @@
 class ItemsController < ApplicationController
 
   def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
   end
 
 end
