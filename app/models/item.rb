f205b4765119a1080808eff308ce532287f0d578
Ta-ho113 <takuma.04.soka@gmail.com>
2022-04-21 03:21:05 +0900

    バリデーション修正2

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,7 +15,7 @@ class Item < ApplicationRecord
   validates        :postage_id,       numericality: { other_than: 1 }
   validates        :region_id,        numericality: { other_than: 1 }
   validates        :shipping_date_id, numericality: { other_than: 1 }
-  validates        :price,            presence: true, inclusion: { in: 300..9_999_999 }, format: { with: /\A[0-9]+\z/ }
+  validates        :price,            presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },format: { with: /\A[0-9]+\z/ }
   #has_many         :comments,         dependent: :destroy
   #has_one          :purchased_item,   dependent: :destroy
 end
