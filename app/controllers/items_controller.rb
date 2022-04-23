823ceb19419cb3a1373da53b4a38ef85da36addc
Ta-ho113 <takuma.04.soka@gmail.com>
2022-04-23 17:28:08 +0900

    レビュー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
+  before_action :set_item, only: [:show, :edit, :update]
   before_action :ensure_current_user, only: [:edit, :update]
-  before_action :set_item, only: [:show, :edit, :update] 
 
   def index
     @items = Item.order(id: :DESC)
@@ -44,7 +44,6 @@ class ItemsController < ApplicationController
   end
 
   def ensure_current_user
-    @item = Item.find(params[:id])
     if current_user.id != @item.user_id
       redirect_to root_path
     end
