fc6bdf6dc017fea355b45dd45ba39f0722a6425c
Ta-ho113 <takuma.04.soka@gmail.com>
2022-04-29 15:08:56 +0900

    バリデーション実装3

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,7 @@
 class OrdersController < ApplicationController
-  before_action :authenticate_user!, only: [:index, :create]
-  before_action :set_item, only: [:index, :create]
-
+  before_action :authenticate_user!
+  before_action :set_item
+  before_action :ensure_current_user
   def index
     @buyers_order = BuyersOrder.new
   end
@@ -9,6 +9,7 @@ class OrdersController < ApplicationController
   def create
     @buyers_order = BuyersOrder.new(order_params)
     if @buyers_order.valid?
+      pay_item
       @buyers_order.save
       return redirect_to root_path
     else
@@ -21,8 +22,23 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
   end
 
+  def ensure_current_user
+    if current_user.id == @item.user_id || @item.order
+      redirect_to root_path
+    end
+  end
+
   def order_params
-    params.require(:buyers_order).permit(:post_number, :region_id, :municipality, :house_number, :building_name, :phone_number).merge(user_id: current_user.id)
+    params.require(:buyers_order).permit(:post_number, :region_id, :municipality, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
   end
 
 end
--- app/models/buyers_order.rb
+++ app/models/buyers_order.rb
@@ -1,24 +1,21 @@
 class BuyersOrder
   include ActiveModel::Model
-  attr_accessor :post_number, :region_id, :municipality, :house_number, :building_name, :phone_number, :user_id, :item_id
+  attr_accessor :post_number, :region_id, :municipality, :house_number, :building_name, :phone_number, :user_id, :item_id, :token
   JAPANESE_CHARACTER = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/
 
   with_options presence: true do
     validates :user_id
     validates :item_id
+    validates :token
     validates :post_number,  format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :municipality, format: {with: JAPANESE_CHARACTER}
-    validates :house_number, format: {with: JAPANESE_CHARACTER}
+    validates :house_number
     validates :phone_number, format: {with: /\A[0-9]+\z/}
   end
   validates :region_id,      numericality: {other_than: 1}
-  validates :building_name,  format: {with: JAPANESE_CHARACTER}
 
   def save
-    # 寄付情報を保存し、変数donationに代入する
     order = Order.create(item_id: item_id, user_id: user_id)
-    # 住所を保存する
-    # donation_idには、変数donationのidと指定する
-    Buyer.create(post_number: post_number, region_id: region.id, municipality: municipality, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: order.id)
+    Buyer.create(post_number: post_number, region_id: region_id, municipality: municipality, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: order.id)
   end
 end
